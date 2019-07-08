68aabf881be2d95bc97733b2b339a6bc22d59851
戴平 <dp0130@gmail.com>
2019-07-08 15:21:17 +0800

    add default tenant

--- kerberos/sql/escheduler_dml.sql
+++ kerberos/sql/escheduler_dml.sql
@@ -2,6 +2,7 @@ USE `escheduler`;
 -- Records of t_escheduler_user,user : admin , password : escheduler123
 INSERT INTO `t_escheduler_user` VALUES ('1', 'admin', '055a97b5fcd6d120372ad1976518f371', '0', 'zhangliye@encdata.cn', '15900532616', '0', now(), now(), NULL);
 INSERT INTO `t_escheduler_alertgroup` VALUES (1, 'escheduler管理员告警组', '0', 'escheduler管理员告警组',now(), now());
+INSERT INTO `t_escheduler_tenant` VALUES (1, 'enncloud', 'enncloud', 'enncloud默认租户', 1, now(), now());
 INSERT INTO `t_escheduler_relation_user_alertgroup` VALUES ('1', '1', '1', now(), now());
 
 -- Records of t_escheduler_queue,default queue name : default
