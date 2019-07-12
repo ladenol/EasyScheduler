c30a8772432d54727ca343decafc64826eb97991
戴平 <dp0130@gmail.com>
2019-07-12 15:00:47 +0800

    add env

--- kerberos/cephutils/entry/entrypoint.sh
+++ kerberos/cephutils/entry/entrypoint.sh
@@ -4,10 +4,13 @@ cd /opt/mntcephutils/entry/
 
 cp install.sh /opt/escheduler/
 cp monitor-logger.sh /opt/escheduler/
+cp escheduler_env.sh /opt/escheduler/conf/env/.escheduler_env.sh
 
 cd /opt/escheduler/
 
 ./install.sh
+
+cp /opt/escheduler/conf/*-site.xml /opt/spark/conf/
 
 if [ ${BD_ESCHEDULER_ROLE} = "worker-server" ]; then
     ./bin/escheduler-daemon.sh start logger-server
---
+++ kerberos/cephutils/entry/escheduler_env.sh
@@ -0,0 +1,4 @@
+export SPARK_HOME=/opt/spark
+export JAVA_HOME=/usr/local/jdk-8u131
+
+export PATH=$SPARK_HOME/bin:$JAVA_HOME/bin:$PATH
