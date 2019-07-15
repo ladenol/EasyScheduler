cae4e83a4e9280c58dfbcb979f8dbb6dab98510e
戴平 <dp0130@gmail.com>
2019-07-15 15:30:55 +0800

    remove empty line

--- escheduler-ui/src/js/conf/home/pages/resource/pages/file/pages/details/index.vue
+++ escheduler-ui/src/js/conf/home/pages/resource/pages/file/pages/details/index.vue
@@ -79,7 +79,7 @@
             this.isData = false
           } else {
             this.isData = true
-            this._handlerEditor().setValue(res.data.content + '\n')
+            this._handlerEditor().setValue(res.data.content)
 
             // Initialize the plugin to prevent repeated calls
             if (editor.lineCount() < 1000) {
